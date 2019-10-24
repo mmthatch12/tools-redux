0d52d7cfd77a966e013ace10d77d1203ab76d00d
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-10-24 08:41:48 -0600

    added react-router, wrapped app in router

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import { reducer } from './reducer'
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>,
+    document.getElementById('root')
+);
 
 
