df67fc4af0fbd4ea9604bf40c1505e53b0704f6b
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-10-28 07:28:43 -0600

    got the page to load, had to install a new package and fix a few things on the actions

--- src/actions/toolActions.js
+++ src/actions/toolActions.js
@@ -7,17 +7,16 @@ export const GET_TOOL_DATA_FAILURE = 'GET_TOOL_DATA_FAILURE'
 export const getInfo = () => {
     return dispatch => {
         dispatch({ type: GET_TOOL_DATA_START })
-        axios.get()
+        axios.get(`https://www.balldontlie.io/api/v1/players`)
             .then(res => {
                 console.log("res", res.data.data)
-                dispatch({ type: Get_Tool_Data_Success, payload: res.data.data})
+                dispatch({ type: GET_TOOL_DATA_SUCCESS, payload: res.data.data})
             })
             .catch(err => {
                 dispatch({ type:
-                GET_Tool_Data_FAILURE,
+                GET_TOOL_DATA_FAILURE,
                 payload: err.response
                 })
-                
             })
     }
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import { reducer } from './reducer'
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
     <Router>
