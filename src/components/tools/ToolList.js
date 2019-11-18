a6312770fd48462ca97a6fe245621db372caf053
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-11-18 11:00:45 -0700

    Got the availableTools data to display!

--- src/components/tools/ToolList.js
+++ src/components/tools/ToolList.js
@@ -6,22 +6,21 @@ import { availableTools } from '../../actions/toolActions'
 const ToolList = (props) => {
     const[test, setTest] = useState([])
 
-    const handleClick = e => {
-        e.preventDefault()
-        props.availableTools()
-    }
-
-    // useEffect(() => {
+    // const handleClick = e => {
+    //     e.preventDefault()
     //     props.availableTools()
-    // }, [])
+    // }
 
-    console.log('***props from ToolList', props.tools[0])
+    useEffect(() => {
+        props.availableTools()
+    }, [])
+
+    console.log('***props from ToolList', props.tools)
 
     return (
         <>
             <div>Not working yet?</div>
-            <button onClick={handleClick}>Work?</button>
-            {/* {props.tools[0].map(tool => <div>{tool.name}</div>)} */}
+            {props.tools && props.tools.map(tool => tool.value.map((tooly, ind) => <div key={ind}>{tooly.name}</div>))}
         </>
         
     )
