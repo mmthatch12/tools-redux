a2c243d51a9b0a809f33dd50f61d60f44ba4fce1
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-11-25 10:09:53 -0700

    working on UserDash.js

--- src/components/tools/ToolList.js
+++ src/components/tools/ToolList.js
@@ -1,33 +1,3 @@
-// import React, { useState, useEffect } from 'react'
-// import { withRouter } from "react-router-dom";
-// import { connect } from "react-redux";
-// import { availableTools } from '../../actions/toolActions'
-
-// const ToolList = (props) => {
-//     const[test, setTest] = useState([])
-
-//     useEffect(() => {
-//         props.availableTools()
-//     }, [])
-
-//     console.log('***props from ToolList', props.tools)
-
-//     return (
-//         <>
-//             <div>Not working yet?</div>
-//             {props.tools && props.tools.map(tool => tool.value.map((tooly, ind) => <div key={ind}>{tooly.name}</div>))}
-//         </>
-        
-//     )
-// }
-
-// const mapStateToProps = state => ({
-//     isLoading: state.isLoading,
-//     tools: state.tools
-//   });
-  
-//   export default withRouter(connect(mapStateToProps, { availableTools })(ToolList))
-
 import React, { useEffect } from 'react';
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
--- src/components/tools/UserDash.js
+++ src/components/tools/UserDash.js
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
 
 const UserDash = (props) => {
-    return (
-        <>
-            <div>From UserDash.js</div>
-        </>
-    )
+  const classes = useStyles();
+
+    // useEffect(() => {
+    //     props.availableTools()
+    // }, [])
+
+  return (
+    <>
+        {props.tools && props.tools.map(tool => tool.value.map((tooly, ind) => (        
+            <Card className={classes.card} key={ind}>
+                <CardActionArea>
+                    <CardMedia
+                    className={classes.media}
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Tool: Practice
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Owner: Practice
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                    Share
+                    </Button>
+                    <Button size="small" color="primary">
+                    Learn More
+                    </Button>
+                </CardActions>
+            </Card>)
+        ))}
+
+    </>
+
+
+  );
 }
 
-export default UserDash
+const mapStateToProps = state => ({
+    isLoading: state.isLoading,
+    tools: state.tools
+  });
+  
+export default withRouter(connect(mapStateToProps, {  })(UserDash))
